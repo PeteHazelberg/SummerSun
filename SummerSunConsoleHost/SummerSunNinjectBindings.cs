6885bbacc33345a43676c720fd39c4eb2fab6dbc
PeteHazelberg <pete_hazelberg@yahoo.com>
2014-03-25 21:33:38 -0500

    Remove client id/secret in advance of Github push

--- SummerSunConsoleHost/SummerSunNinjectBindings.cs
+++ SummerSunConsoleHost/SummerSunNinjectBindings.cs
@@ -9,8 +9,10 @@ namespace SummerSun
     {
         public override void Load()
         {
-            var clientId = Settings.Get("JciClientId", null);
-            var clientSecret = Settings.Get("JciClientSecret", null);
+            var clientId = Settings.Get("JciClientId", null, s => !string.IsNullOrEmpty(s), "You must specify a JCI-issued client id in the .config file.");
+            var clientSecret = Settings.Get("JciClientSecret", null, s => !string.IsNullOrEmpty(s), "You must specify a JCI-issued client secret in the .config file.");
+            if (string.IsNullOrEmpty(clientId) || string.IsNullOrEmpty(clientSecret))
+                throw new ArgumentException("JciClientId and/or JciClientSecret is not valid.");
             var tokenEndpoint = Settings.Get("JciTokenEndpoint", "https://identity.johnsoncontrols.com/issue/oauth2/token",
                 s => new Uri(s).IsAbsoluteUri,
                 "JciTokenEndpoint in configuration settings should be the URL where JCI's tokens are issued from.");
