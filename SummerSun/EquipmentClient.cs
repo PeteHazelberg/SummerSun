5a41f8008baa5a33227712c9d9e85d399acd63df
PeteHazelberg <pete_hazelberg@yahoo.com>
2014-03-25 21:50:07 -0500

    Add README

---
+++ SummerSun/EquipmentClient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Flurl;
+
+namespace BuildingApi
+{
+    public class EquipmentClient
+    {
+        public IEnumerable<Equipment> GetEquipmentAndPointRoles(Guid customerId, string equipmentType, string pointRoleType, int offset = 0, int max = 250)
+        {
+            var url = apiBaseUrl.AppendPathSegment("equipment").SetQueryParams(new
+            {
+                type = equipmentType,
+                _expand = "pointRoles",
+                _offset = offset.ToString(CultureInfo.InvariantCulture),
+                _limit = max.ToString(CultureInfo.InvariantCulture)
+            }).ToString();
+            var resp = HttpHelper.Get<Page<Equipment>>(customerId, url, tokens);
+            return (resp == null || resp.Items == null) ? new List<Equipment>() : resp.Items;
+        }
+
+        public IEnumerable<Point> GetPointsAndSummary(Guid customerGuid, IEnumerable<string> pointIds)
+        {
+            var url = apiBaseUrl.AppendPathSegment("points").SetQueryParams(new
+            {
+                id = string.Join(",", pointIds),
+                _expand = "sampleSummary",
+                _fields = "sampleSummary.updated",
+                _offset = "0"
+            }).ToString();
+            var resp = HttpHelper.Get<Page<Point>>(customerGuid, url, tokens);
+            return (resp == null || resp.Items == null) ? new List<Point>() : resp.Items;            
+        }
+
+        public EquipmentClient(ITokenProvider tokenProvider, string buildingApiUrl)
+        {
+            this.tokens = tokenProvider;
+            this.apiBaseUrl = buildingApiUrl;
+        }
+
+        private readonly string apiBaseUrl;
+        private readonly ITokenProvider tokens;
+    }
+}
