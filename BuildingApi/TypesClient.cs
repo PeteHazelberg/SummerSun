c88db29830d7e5ed23bfc18ab719f6e88c0e4e3f
svigano <simo.vigano@gmail.com>
2014-03-30 11:50:37 -0500

    Expose the EquipmetType endpoint

---
+++ BuildingApi/TypesClient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Flurl;
+
+
+namespace BuildingApi
+{
+    public class TypesClient
+    {
+        private readonly string _apiBaseUrl;
+        private readonly ITokenProvider _tokenProvider;
+
+        public TypesClient(ITokenProvider tokenProvider, string buildingApiUrl)
+        {
+            this._tokenProvider = tokenProvider;
+            this._apiBaseUrl = buildingApiUrl;
+        }
+
+        public IEnumerable<EquipmentType> GetEquipmentTypes(Company c)
+        {
+            var token = _tokenProvider.Get(c);
+
+            var url = _apiBaseUrl.AppendPathSegment("types/Equipment");
+            var resp = HttpHelper.Get<Page<EquipmentType>>(url, token);
+            return (resp == null || resp.Items == null) ? new List<EquipmentType>() : resp.Items;
+        }
+    }
+}
