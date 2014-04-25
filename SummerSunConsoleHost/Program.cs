f5a0242857ce82f31ed68398236ea370576116da
PeteHazelberg <pete_hazelberg@yahoo.com>
2014-04-25 15:23:24 -0500

    Add SampleSummary.Newest display, improved text rendering, and robustness checks

--- SummerSunConsoleHost/Program.cs
+++ SummerSunConsoleHost/Program.cs
@@ -6,6 +6,7 @@ using System.Net.Http;
 using BuildingApi;
 using Mono.Options;
 using Ninject;
+using Humanizer;
 
 namespace SummerSun
 {
@@ -29,10 +30,10 @@ namespace SummerSun
             Console.WriteLine("INPUT: CompanySearch= {0} EquipmentType={1}  PointRoleType={2}  pageSize={3}", companySearch, equipmentType, pointRoleType, pageSize);
 
             var company = PromptToChooseCompany(kernel, companySearch);
-
-            var sun = kernel.Get<EquipmentClient>();
             Console.WriteLine("Searching company {0} ...", company.Name);
+
             var equip = new Dictionary<string, Equipment>();
+            var sun = kernel.Get<EquipmentClient>();
             var watch = new Stopwatch();
             watch.Start();
             try
@@ -49,8 +50,8 @@ namespace SummerSun
             }
             watch.Stop();
             Console.WriteLine("Found {0} equipment with \"{1}\" in their type ({2}ms)", equip.Count(), equipmentType, watch.ElapsedMilliseconds);
+            
             var equip2Roles = new Dictionary<string, IList<PointRole>>();
-            var pointId2Equip = new Dictionary<string, string>();
             foreach (var e in equip.Values)
             {
                 foreach (var r in e.PointRoles.Items.Where(r => r.Type.Id.ToLowerInvariant().Contains(pointRoleType.ToLowerInvariant())))
@@ -65,29 +66,40 @@ namespace SummerSun
                     {
                         equip2Roles.Add(e.Id, new List<PointRole> { r });
                     }
-                    pointId2Equip.Add(r.Point.Id, e.Id);
                 }
             }
+            
             IDictionary<string, Point> role2Point = new Dictionary<string, Point>();
             if (equip2Roles.Any())
             {
-                var ptIds = new HashSet<string>((from roleList in equip2Roles.Values from role in roleList select role.Point.Id));
+                var ptIds = (from roleList in equip2Roles.Values from role in roleList select role.Point.Id).ToList();
+                var pts = new List<Point>();
+                var numOfRequests = 0;
                 watch.Restart();
-                var pts = sun.GetPointsAndSummary(ptIds, company).ToList();
+                for (int i = 0; i < ptIds.Count; i += MaxPointIdsInQueryString)
+                {
+                    numOfRequests++;
+                    pts.AddRange(sun.GetPointsAndSummary(ptIds.GetRange(i, Math.Min(MaxPointIdsInQueryString, ptIds.Count - i)), company));
+                }
                 watch.Stop();
-                Console.WriteLine("Found {0} points on those equipment with \"{1}\" in role type ({2}ms)", pts.Count(), pointRoleType, watch.ElapsedMilliseconds); 
+                Console.WriteLine("Found {0} points on those equipment with \"{1}\" in role type ({2}ms across {3} HTTP GET requests)", pts.Count(), pointRoleType, watch.ElapsedMilliseconds, numOfRequests); 
                 foreach (var pt in pts)
                 {
                     var ptId = pt.Id;
-                    foreach (var role in equip.Values.Where(eq => eq.PointRoles != null && eq.PointRoles.Items != null).SelectMany(eq => eq.PointRoles.Items.Where(r => r.Point.Id == ptId)))
+                    foreach (var role in equip.Values.Where(eq => eq.PointRoles != null && eq.PointRoles.Items != null).SelectMany(eq => eq.PointRoles.Items.Where(r => r.Point.Id == ptId)).Where(role => !role2Point.ContainsKey(role.Id)))
                     {
                         role2Point.Add(role.Id, pt);
                     }
                 }
             }
-            const string tablePattern = "|{0,-40}|{1,-40}|{2,-20}|{3,12}|{4,12}|{5,12}|";
+            else
+            {
+                Console.WriteLine("No points found on those equipment with \"{0}\" in their role type.", pointRoleType);
+            }
+            
+            const string tablePattern = "|{0,-30}|{1,-30}|{2,15}|{3,-15}|{4,-15}|";
             DrawTableLine(tablePattern);
-            Console.WriteLine(tablePattern, '"' + equipmentType + '"', '"' + pointRoleType + '"', "Units", "Sample Count", "Minimum", "Maximum");
+            Console.WriteLine(tablePattern, '"' + equipmentType + '"', '"' + pointRoleType + '"', "Newest Value", "Units", "When");
             DrawTableLine(tablePattern);
 
             foreach (var eId in equip.Keys)
@@ -100,34 +112,71 @@ namespace SummerSun
                         if (role2Point.ContainsKey(role.Id))
                         {
                             var pt = role2Point[role.Id];
-                            Console.WriteLine(tablePattern, equip[eId].Name, role.Type.Id, pt.Units.Id ?? pt.States.Id,
-                                pt.SampleSummary.Count, pt.SampleSummary.MinValue.ToString("F1"), pt.SampleSummary.MaxValue.ToString("F1"));
+                            Console.WriteLine(tablePattern, FormatEquipmentName(equip[eId]), FormatRoleTypeName(role), 
+                                ExtractNewestVal(pt), ExtractUnits(pt), ExtractNewestTimeStamp(pt));
                         }
                         else
                         {
-                            Console.WriteLine(tablePattern, equip[eId].Name, role.Id, "(not found)", "(not found)", "(not found)", "(not found)");
+                            Console.WriteLine(tablePattern, FormatEquipmentName(equip[eId]), FormatRoleTypeName(role), " (not found)", " (not found)", " (not found)");
                         }
                     }
                 }
                 else
-                    Console.WriteLine(tablePattern, equip[eId].Name, string.Empty, string.Empty, string.Empty, string.Empty, string.Empty);
+                    Console.WriteLine(tablePattern, FormatEquipmentName(equip[eId]), "  ---", string.Empty, string.Empty, string.Empty);
             }
             DrawTableLine(tablePattern);
             Console.ReadKey();
         }
 
+        private static string FormatEquipmentName(Equipment eq)
+        {
+            if (eq == null || eq.Name == null) { return string.Empty; }
+            return eq.Name.Truncate(30);
+        }
+        private static string FormatRoleTypeName(PointRole role)
+        {
+            if (role == null || role.Type == null || role.Type.Id == null) { return string.Empty; }
+            return role.Type.Id.Humanize(LetterCasing.Title).Truncate(30);
+        }
+        private static string ExtractNewestTimeStamp(Point pt)
+        {
+            if (pt == null || pt.SampleSummary == null || pt.SampleSummary.Newest == null) { return string.Empty; }
+            return pt.SampleSummary.Newest.Timestamp.Humanize();
+        }
+        private static string ExtractNewestVal(Point pt)
+        {
+            if (pt == null || pt.SampleSummary == null || pt.SampleSummary.Newest == null) {  return string.Empty; }
+            return pt.SampleSummary.Newest.Value.ToString("F1");
+        }
+        private static string ExtractUnits(Point pt)
+        {
+            if (pt == null) { return string.Empty; }
+            if (pt.Units == null && pt.States == null) { return string.Empty; }
+            var retVal = pt.Units == null ? pt.States.Id : pt.Units.Id;
+            if (retVal.StartsWith("enumset", StringComparison.InvariantCultureIgnoreCase)) { retVal = retVal.Remove(0, 7); }
+            return retVal.Humanize(LetterCasing.Title).Truncate(15);
+        }
+
         private static void DrawTableLine(string tablePattern)
         {
-            const string dashes20 = "--------------------";
-            const string dashes40 = dashes20 + dashes20;
-            const string dashes12 = "------------";
-            Console.WriteLine(tablePattern, dashes40, dashes40, dashes20, dashes12, dashes12, dashes12);
+            const string dashes15 = "---------------";
+            const string dashes30 = dashes15 + dashes15;
+            Console.WriteLine(tablePattern, dashes30, dashes30, dashes15, dashes15, dashes15, dashes15);
         }
 
         private static Company PromptToChooseCompany(IKernel kernel, string companySearch)
         {
             var compRepo = kernel.Get<ICompanyProvider>();
-            var allCompanies = compRepo.Get().ToList();
+            var allCompanies = new List<Company>();
+            try
+            {
+                allCompanies = compRepo.Get().ToList();
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.ToString());
+                Environment.Exit(1);
+            }
             int ci, num = 0;
             var compIndex = allCompanies.Where(c => c.Name.ToLowerInvariant().Contains(companySearch.ToLowerInvariant())).ToDictionary(c => ++num);
             if (compIndex.Count == 1)
@@ -152,5 +201,7 @@ namespace SummerSun
             }
             return compIndex[ci];
         }
+
+        private const int MaxPointIdsInQueryString = 50;
     }
 }
