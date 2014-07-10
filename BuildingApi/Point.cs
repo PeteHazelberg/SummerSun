5603095204ae9e2901e2267eeb8b6999ca69745a
PeteHazelberg <pete_hazelberg@yahoo.com>
2014-07-10 08:50:08 -0500

    Add preliminary support for Attributes to Point

--- BuildingApi/Point.cs
+++ BuildingApi/Point.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using Newtonsoft.Json.Linq;
 
 namespace BuildingApi
 {
@@ -10,7 +11,17 @@ namespace BuildingApi
         public EntityLink Units { get; set; }
         public EntityLink States { get; set; }
         public SampleSummary SampleSummary { get; set; }
-        public Link Attributes { get; set; }
+        public JObject Attributes { get; set; }
+
+        public string GetAttribute(string group, string name)
+        {
+            if (this.Attributes == null
+                || this.Attributes[group] == null
+                || this.Attributes[group]["attributes"] == null
+                || this.Attributes[group]["attributes"][name] == null) return string.Empty;
+
+            return this.Attributes[group]["attributes"].Value<string>(name);
+        }
 
         public override bool Equals(object obj)
         {
