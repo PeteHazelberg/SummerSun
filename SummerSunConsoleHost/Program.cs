6564f2a4adaf8c58149b61661765e647e8ebad0e
PeteHazelberg <PeteHazelberg@users.noreply.github.com>
2014-07-11 11:30:21 -0500

    ConsoleApp: Default to no filtering
    EquipmentType and PointRoleType

--- SummerSunConsoleHost/Program.cs
+++ SummerSunConsoleHost/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -15,8 +15,8 @@ namespace SummerSun
         static void Main(string[] args)
         {
             string companySearch = "";
-            string equipmentType = "Chiller";
-            string pointRoleType = "ChillerStatus";
+            string equipmentType = "";
+            string pointRoleType = "";
             int pageSize = 10;
 
             var commandLineParams = new OptionSet()
