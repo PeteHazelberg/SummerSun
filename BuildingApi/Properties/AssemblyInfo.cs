5e9e50ad7c681b375c35d88deb058e8578765d2e
PeteHazelberg <pete_hazelberg@yahoo.com>
2014-07-28 11:21:02 -0500

    Building API - Add nuspec

--- BuildingApi/Properties/AssemblyInfo.cs
+++ BuildingApi/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SummerSun")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("BuildingApi")]
+[assembly: AssemblyDescription("A helper library to ease .NET client interactions with Johnson Controls' Building API.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Johnson Controls")]
-[assembly: AssemblyProduct("SummerSun")]
-[assembly: AssemblyCopyright("Copyright © Johnson Controls 2014")]
+[assembly: AssemblyCompany("Pete Hazelberg")]
+[assembly: AssemblyProduct("BuildingApi")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.*")]
