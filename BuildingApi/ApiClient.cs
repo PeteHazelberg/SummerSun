dc3d2567664f652368a674d234462e7457b2c548
PeteHazelberg <pete_hazelberg@yahoo.com>
2014-07-28 13:06:14 -0500

    BuildingApi - add ApiClient to replace the too-specific EquipmentClient, TypesClient, and CompanyClient

---
+++ BuildingApi/ApiClient.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Flurl;
+
+namespace BuildingApi
+{
+    public class ApiClient
+    {
+        private readonly string baseUrl;
+        private readonly ITokenProvider tokens;
+        public ApiClient(ITokenProvider tokenProvider, string baseUrl)
+        {
+            this.tokens = tokenProvider;
+            this.baseUrl = baseUrl;
+        }
+
+        public string BaseUrl { get { return baseUrl; } }
+
+        public IEnumerable<T> GetPage<T>(Url url, Company company)
+        {
+            var resp = HttpHelper.Get<Page<T>>(company, url.ToString(), tokens);
+            return (resp == null || resp.Items == null) ? new List<T>() : resp.Items;
+        }
+
+        public List<Company> GetCompanies()
+        {
+            var token = this.tokens.Get();
+            return HttpHelper.Get<List<Company>>(BaseUrl.AppendPathSegment("companies").ToString(), token);
+        }
+    }
+}
