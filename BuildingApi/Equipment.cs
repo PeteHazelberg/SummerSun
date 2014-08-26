b1e1fd13964c064bea4ba25c31b8f52f7d9e1ef2
ploegert <superploge@gmail.com>
2014-08-26 13:27:11 -0500

    Added Equipment attributes for servesEquip/ServedbyEquip/servesspaces

--- BuildingApi/Equipment.cs
+++ BuildingApi/Equipment.cs
@@ -1,12 +1,25 @@
 ﻿using Newtonsoft.Json;
+using System.Collections.Generic;
 
 namespace BuildingApi
 {
     public class Equipment : Entity
     {
         public EntityLink Type { get; set; }
+        
         [JsonProperty(PropertyName = "isLocatedInSpace")]
         public EntityLink Location { get; set; }
+
+        [JsonProperty(PropertyName = "servesEquipment")]
+        public Page<Equipment> servesEquipment { get; set; }
+
+        [JsonProperty(PropertyName = "isServedByEquipment")]
+        public Page<Equipment> isServedByEquipment { get; set; }
+
         public Page<PointRole> PointRoles { get; set; }
+
+        [JsonProperty(PropertyName = "servesSpaces")]
+        public Page<Space> servesSpaces { get; set; }
+        
     }
 }
